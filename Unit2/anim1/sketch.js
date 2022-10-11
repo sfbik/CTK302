ed157dbcebd68922cd8508efecef23ebdd4ae47a
sfbik <ahero.intl@gmail.com>
2022-10-11 16:19:32 -0500

    Unit2

--- Unit2/anim1/sketch.js
+++ Unit2/anim1/sketch.js
@@ -1,14 +1,19 @@
+let x = 0;
+let f1;
+
 function setup() {
   createCanvas(500, 500);
+  f1 = loadFont("assets/theory.otf");
+  textAlign(CENTER);
 }
 
 function draw() {
   background(100);
-  fill('black');
-  textSize(56);
-  rect("Box", x, 100, 100, 100);
+  fill('yellow');
+  textFont(f1, 56);
+  text("Epic", x, 200, 100, 100);
   x += 5;
-  if (x > width) {
-    x = 0;
+  if (x > 550) {
+    x = -100;
   }
 }
